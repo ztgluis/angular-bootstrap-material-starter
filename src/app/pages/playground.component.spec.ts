f49a4e3cd2e481cfad6d2d72993b0945cc1b4e0f
Luis Gutierrez <ztgluis@gmail.com>
2018-10-02 22:33:33 -0500

    Add test file to playground component

---
+++ src/app/pages/playground.component.spec.ts
@@ -0,0 +1,27 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PlaygroundComponent } from './playground.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+
+describe('PlaygroundComponent', () => {
+    let component: PlaygroundComponent;
+    let fixture: ComponentFixture<PlaygroundComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [PlaygroundComponent],
+            schemas: [NO_ERRORS_SCHEMA]
+        })
+            .compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(PlaygroundComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
