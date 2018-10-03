10fc5148dfd0354cfb9ad0d97c58912207e349f1
Luis Gutierrez <ztgluis@gmail.com>
2018-10-02 23:52:44 -0500

    Added dummy home component/module

---
+++ src/app/pages/home/home.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from '@app/shared';
+import { HomeComponent } from './home.component';
+
+@NgModule({
+    imports: [
+        SharedModule,
+        RouterModule.forChild([
+            {
+                path: '',
+                component: HomeComponent
+            }
+        ])
+    ],
+    declarations: [
+        HomeComponent
+    ]
+})
+
+export class HomeModule { }
