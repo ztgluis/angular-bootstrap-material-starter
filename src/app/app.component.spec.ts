bd87e57e0fcddebfe16267e3716c125227e674f6
Luis Gutierrez <ztgluis@gmail.com>
2019-02-22 23:34:26 -0600

    Add NO_ERROR_SCHEMA to tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,15 @@
 import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
+
+import { AppComponent } from './app.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('AppComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            imports: [
-                RouterTestingModule
-            ],
-            declarations: [
-                AppComponent
-            ],
+            imports: [RouterTestingModule],
+            declarations: [AppComponent],
+            schemas: [NO_ERRORS_SCHEMA]
         }).compileComponents();
     }));
     it('should create the app', async(() => {
