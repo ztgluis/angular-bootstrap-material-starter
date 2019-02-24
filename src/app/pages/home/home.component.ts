de1aa32641a3e4d2b9ff6b0d78cd57902e271b33
Luis Gutierrez <ztgluis@gmail.com>
2019-02-24 00:39:02 -0600

    Theming: Enable multiple themes
    Enables multiple themes to be generated with consistent styles between Material and Bootstrap.
    Enables Lazy loading of css theme files.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'ngbm-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'ngbm-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    defaultTheme = true;
+
+    switchTheme() {
+        let theme;
+
+        if (this.defaultTheme) {
+            theme = 'material-dark-theme.css';
+        } else {
+            theme = 'material-default-theme.css';
+        }
+
+        this.defaultTheme = !this.defaultTheme;
+
+        window.localStorage.setItem('theme', JSON.stringify(theme));
+        let link = document.querySelector('link[href*="material-"]');
+
+        link['href'] = theme;
+    }
+
+    ngOnInit() {
+    }
 
 }
