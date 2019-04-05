249e3d6df249f96f01a23394401c5f7e557cff01
Luis Gutierrez <ztgluis@gmail.com>
2019-04-05 00:11:56 -0500

    Add storage service

---
+++ src/app/shared/services/storage.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class StorageService {
+    get(key: string): any {
+        const value = localStorage.getItem(key);
+        try {
+            return JSON.parse(value);
+        } catch (e) {
+            return value;
+        }
+    }
+
+    set(key: string, input: any): void {
+        const value = typeof input === 'string' ? input : JSON.stringify(input);
+        localStorage.setItem(key, value);
+    }
+
+    remove(key: string): void {
+        localStorage.removeItem(key);
+    }
+
+    clear() {
+        localStorage.clear();
+    }
+}
