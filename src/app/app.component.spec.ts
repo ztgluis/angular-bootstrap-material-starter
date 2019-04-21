57a401dc13f55658507993fca07eddfeac8f689b
Luis Gutierrez <ztgluis@gmail.com>
2019-04-21 16:43:59 -0500

    Add settingsService spy on app.component.spec

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,20 +3,30 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatMenuModule } from '@angular/material';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { SettingsService } from './shared/services/settings.service';
 
 describe('AppComponent', () => {
     let component: AppComponent;
     let fixture: ComponentFixture<AppComponent>;
+    let settingsServiceSpy: jasmine.SpyObj<SettingsService>;
 
     beforeEach(async(() => {
+        const spy = jasmine.createSpyObj('SettingsService', [
+            'switchTheme',
+            'setLayout',
+            'getLayout'
+        ]);
+
         TestBed.configureTestingModule({
             imports: [RouterTestingModule, MatMenuModule],
             declarations: [AppComponent],
+            providers: [{ provide: SettingsService, useValue: spy }],
             schemas: [NO_ERRORS_SCHEMA]
         }).compileComponents();
 
         fixture = TestBed.createComponent(AppComponent);
         component = fixture.debugElement.componentInstance;
+        settingsServiceSpy = TestBed.get(SettingsService);
     }));
 
     it('should create', () => {
@@ -28,6 +38,8 @@ describe('AppComponent', () => {
             fixture.detectChanges();
             expect(component.sidenavLayout).toBeFalsy();
             component.switchLayout();
+            expect(component.sidenavLayout).toBeTruthy();
+            expect(settingsServiceSpy.setLayout).toHaveBeenCalled();
         }));
     });
 
@@ -35,6 +47,7 @@ describe('AppComponent', () => {
         it('should switch the theme', async(() => {
             fixture.detectChanges();
             component.switchTheme();
+            expect(settingsServiceSpy.switchTheme).toHaveBeenCalled();
         }));
     });
 
