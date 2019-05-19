026a6a08a8eed5b222cbd6550b7b3f715df6eeb9
Luis Gutierrez <ztgluis@gmail.com>
2019-05-18 23:12:55 -0500

    Redesign Playground component

--- src/app/pages/playground/playground.component.ts
+++ src/app/pages/playground/playground.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
     selector: 'ngbm-playground',
@@ -7,13 +6,9 @@ import { FormControl, FormGroup } from '@angular/forms';
     styleUrls: ['./playground.component.scss']
 })
 export class PlaygroundComponent {
-    group = new FormGroup({
-        fontSize: new FormControl('')
-    });
-
     matColors = ['primary', 'accent', 'warn'];
 
-    twbsColors = [
+    bootstrapColors = [
         'primary',
         'secondary',
         'info',
