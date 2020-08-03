ee0c076fc697ff8340a25f85b4810cc407577faa
Luis Gutierrez <ztgluis@gmail.com>
2020-08-03 00:16:04 -0500

    Fix issue with Swapi API
    Force HTTP requests to be HTTPS to prevent mixed content errors.

--- src/app/pages/api-testing/swapi/swapi.service.ts
+++ src/app/pages/api-testing/swapi/swapi.service.ts
@@ -6,17 +6,21 @@ const Swapi = {
     host: 'https://swapi.dev/api/'
 };
 
+const replaceHttp = (text) => {
+    return text.replace(/^http:/, 'https:');
+};
+
 @Injectable({
     providedIn: 'root'
 })
 export class SwapiService {
-    constructor(private storageService: StorageService) {}
+    constructor(private storageService: StorageService) { }
 
     getTabs(): Observable<any> {
         return this.storageService.getOrCache(Swapi.host);
     }
 
     getData(url): Observable<any> {
-        return this.storageService.getOrCache(url);
+        return this.storageService.getOrCache(replaceHttp(url));
     }
 }
